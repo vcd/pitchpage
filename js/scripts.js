468a15f203af2e1a240bfc8a3927a745dc663d82
ewuweblab <pmanikoth@ewu.edu>
2016-05-16 16:26:28 -0700

    v1

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// First, load jQuery (required)
+// jQuery 2.2.2 loaded via `index.html`
+
+// Second, load Semantic UI JS (required)
+// @codekit-prepend "semantic.js";
+
+// Third, load any plugins (optional)
+// @codekit-prepend "plugins.js";
+
+// My Scripts
+
+$('button').click(function(){
+    
+    $('.ui.sidebar').sidebar('toggle');
+    
+});
+
